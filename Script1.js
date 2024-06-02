0ac3a37d24a8bf8c75560c62f5f0b533d03ec2fd
Behnooshta <behnoosh.taghdisi.a@gmail.com>
2024-06-02 17:54:19 +0330

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,9 @@
+﻿// JavaScript source code
+var date = new Date();
+document.write(date);
+function message() {
+    alert("درود،  به پت شاپ لیمو خوش آمدید")
+}
+function message2() {
+    alert("سفارشات شما ثبت گردید ،ممنون ازخرید شما")
+}
